dc9da374170ef2cc74638fba606d620c34cb5dee
Patrick Steffanic <collidingpaticles@gmail.com>
2023-11-16 16:09:53 -0500

    Fixing example to be more correct

--- streamlit_geolocation/example.py
+++ streamlit_geolocation/example.py
@@ -4,14 +4,6 @@ from streamlit_geolocation import streamlit_geolocation
 
 st.title("Streamlit Geolocation")
 
-st.write("This is a demo of the Streamlit Geolocation component.")
-
-st.write("The component is defined in `frontend/src/Geolocation.tsx`.")
-
-st.write("The component is used in `example.py`.")
-
-st.write("The component is published to PyPI as `streamlit_geolocation`.")
-
 try:
     loc_string = streamlit_geolocation()
 except:
